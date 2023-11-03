738480d291d3ab4de8c10e1c589a755250f4db4d
ArielDeveloper <alvarez.developer@gmail.com>
2023-11-03 07:06:01 -0300

    cart casi terminada

--- carts/models.py
+++ carts/models.py
@@ -18,5 +18,8 @@ class CartItem(models.Model):
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
-    def __str__(self) -> str:
+    def sub_total(self):
+        return self.product.price * self.quantity
+
+    def __unicode__(self):
         return self.product
